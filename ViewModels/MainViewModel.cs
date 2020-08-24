0ba0ad79eaef4643e97d9449c0b63a8a4ba16058
Russell de Pina <pinaruss@amazon.com>
2020-08-24 17:55:47 +0000

    suppress CS1998 warning for MainViewModel commands

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -26,16 +26,20 @@ namespace WpfFrontEndProto.ViewModels
             SeedDataCommand = new AsyncCommand<object>(ExecuteSeedDataAsync, CanExecute);
         }
 
-        private async Task ExecuteViewDataAsync(object arg = null)
-        {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+		private async Task ExecuteViewDataAsync(object arg = null)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+		{
             IsBusy = true;
             //MessageBox.Show("Show View Data view");
             _viewUtils.ShowCurveDataModalView();
             IsBusy = false;
         }
 
-        private async Task ExecuteSeedDataAsync(object arg = null)
-        {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+		private async Task ExecuteSeedDataAsync(object arg = null)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+		{
             IsBusy = true;
             MessageBox.Show("Show Seed Data view");
             IsBusy = false;
